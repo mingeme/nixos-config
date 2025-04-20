b88b8dd2231000ecb7f6ef4aae11bbef292b839e
mingeme <heminghu00@126.com>
2025-04-20 21:08:10 +0800

    chore: update .gitignore and add path to zsh.nix

--- modules/shared/programs/zsh.nix
+++ modules/shared/programs/zsh.nix
@@ -46,6 +46,8 @@
       . /nix/var/nix/profiles/default/etc/profile.d/nix.sh
     fi
 
+    export PATH="/opt/homebrew/bin:$PATH"
+
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
